3c08e8b8a1880bec0b719e34c00a4f0ee64b15e9
cag185 <cag185@pitt.edu>
2023-10-25 22:49:21 -0400

    Was an actual retard and almost lost all my code

--- Facial-Detection/PCA_compression.py
+++ Facial-Detection/PCA_compression.py
@@ -1,57 +1,93 @@
+# https://towardsdatascience.com/dimensionality-reduction-of-a-color-photo-splitting-into-rgb-channels-using-pca-algorithm-in-python-ba01580a1118
+# this might be helpful in updating the effeciency of PCA
+
 import numpy as np
 from sklearn.decomposition import PCA
 from PIL import Image, ImageOps
 import matplotlib.pyplot as plt 
 import sys # used to get size data
+import cv2
+import math
+import os
 
+# TODO- for each image in the video capture do the PCA dimensionality reduction
 # load in the image
-imgPath = "C:/Users/17578/Desktop/School/Class Files/Fall 2023/ECE 1896 - Senior Design/Facial-Recognition/Video-to-frames/frames/frame0.jpg"
-full_image = Image.open(imgPath).resize((500,500))
+# parent_img_folder = "C:/Users/17578/Desktop/School/Class Files/Fall 2023/ECE 1896 - Senior Design/Facial-Recognition/Video-to-frames/frames/FaceFrames/"
+parent_img_folder = "C:/Users/17578/Desktop/School/Class Files/Fall 2023/ECE 1896 - Senior Design/Facial-Recognition/Video-to-frames/frames/FlowerFrame/"
+img_folder_arr = os.listdir(parent_img_folder)
+frame_count = 0
+# imgPath = "C:/Users/17578/Desktop/School/Class Files/Fall 2023/ECE 1896 - Senior Design/Facial-Recognition/Video-to-frames/frames/FaceFrames/frame145.jpg"
+# imgPath = "C:/Users/17578/Desktop/School/Class Files/Fall 2023/ECE 1896 - Senior Design/Facial-Recognition/Video-to-frames/frames/FlowerFrame/flower.jpg"
+# for i in img_folder_arr:
+    # imgPath = parent_img_folder + i
+    # tab from here down
+imgPath = parent_img_folder + "flower.jpg"
+img = cv2.cvtColor(cv2.imread(imgPath), cv2.COLOR_BGR2RGB)
+# full_image = Image.open(img_I).resize((500,500))
+full_image = cv2.resize(img,(500,500))
 
-print(full_image.format)
-print(full_image.size)
-print(full_image.mode)
+# split the image into RGB values
+blue, green, red  = cv2.split(full_image)
+# divide the data by 255 to normalize the data (0-1)
+norm_blue = blue/255
+norm_green = green/255
+norm_red = red/255
 
-# convert the image to a numpy array
-img_arr = np.asarray(full_image)
+# fit each image using PCA where n_components is the variance we want to achieve
+n_var = .99
+pca_b = PCA(n_components=n_var)
+pca_b.fit(norm_blue)
+trans_pca_b = pca_b.transform(norm_blue)
 
-# convert the 3 channel image to a single channel grey image
-grayscale_arr = np.dot(img_arr[...,:3], [0.2989, 0.5870, 0.1140])
+pca_g = PCA(n_components=n_var)
+pca_g.fit(norm_green)
+trans_pca_g = pca_g.transform(norm_green)
 
-# flatten the image -- need an array of flattened images to pass into pca
-# flattened_img = np.assimg_arr.flatten()
-height, width = grayscale_arr.shape
-flattened_img = grayscale_arr.reshape(-1,1)
-
-# apply PCA
-n_comp = 1 # change this depending on the variance needed
-pca = PCA(n_components=n_comp)
-compressed_img = pca.fit_transform(flattened_img)
-
-# reconstruct the image
-reconstructed_img = pca.inverse_transform(compressed_img)
-reconstructed_img = reconstructed_img.reshape((height, width))
-
-# show the OG, compressed, and reconstucted image
-fig, ax = plt.subplots(1,3)
-
-fig.suptitle("Image compression and reconstruction with N = " + str(n_comp) + " principle components")
-
-subtitle_string = "Original Image \n Size of the image in kb: " + str(sys.getsizeof(img_arr) / 1000)
-ax[0].imshow(img_arr.astype('uint8'))
-ax[0].axis('off')
-ax[0].set_title(subtitle_string, color='red')
-
-subtitle_string = "Compressed Image \n Size of the image in kb: " + str(sys.getsizeof(compressed_img) / 1000)
-ax[1].imshow(compressed_img.reshape((height, width, n_comp)).astype('uint8'))
-ax[1].axis('off')
-ax[1].set_title(subtitle_string, color='blue')
+pca_r = PCA(n_components=n_var)
+pca_r.fit_transform(norm_red)
+trans_pca_r = pca_r.transform(norm_red)
 
 
-subtitle_string = "Reconstructed Image \n Size of the image in kb: " + str(sys.getsizeof(reconstructed_img) / 1000)
-ax[2].imshow(reconstructed_img.astype('uint8'))
-ax[2].axis('off')
-ax[2].set_title(subtitle_string, color='green')
+# print off the contained variance by the three channels
+# print(f"Blue Channel: {sum(pca_b.explained_variance_ratio_)}")
+# print(f"Green Channel: {sum(pca_g.explained_variance_ratio_)}")
+# print(f"Red Channel: {sum(pca_r.explained_variance_ratio_)}")
 
-plt.tight_layout()
+# recombine the images
+b_arr = pca_b.inverse_transform(trans_pca_b)
+g_arr = pca_g.inverse_transform(trans_pca_g)
+r_arr = pca_r.inverse_transform(trans_pca_r)
+
+# merge the photos back to one
+img_reduced = (cv2.merge((b_arr, g_arr, r_arr)))
+# img_to_write = np.clip((img_reduced / img_reduced.max()) * 255, 1, 255).astype(np.uint16)
+
+# DONT NEED TO PLOT AND SHOW EACH IMAGE
+fig = plt.figure(figsize = (10, 7.2))
+fig.add_subplot(121)
+plt.title("Original Image")
+plt.imshow(full_image)
+
+fig.add_subplot(122)
+plt.title("Reduced Image")
+plt.imshow(img_reduced)
+
 plt.show()
+
+# save the file
+# fileDest = "C:/Users/17578/Desktop/School/Class Files/Fall 2023/ECE 1896 - Senior Design/Facial-Recognition/Facial-Profile-Databank/user1/face_%d.jpg" % frame_count
+fileDest = "C:/Users/17578/Desktop/School/Class Files/Fall 2023/ECE 1896 - Senior Design/Facial-Recognition/Facial-Profile-Databank/user2/face_%d.jpg" % frame_count
+
+
+cv2.imwrite(fileDest, img_reduced)
+# Increase the frame counter
+frame_count+=1
+
+# NOT NEEDED RN
+# Get the file size
+size_reconstruct = os.path.getsize(fileDest) / 1024
+print("The size of the reconstructed file (kb): ", size_reconstruct)
+print("The size of the original file (kb): ", os.path.getsize(imgPath) / 1024)
+# get the number of components
+totalNumComp = pca_b.explained_variance_ratio_.shape[0] + pca_g.explained_variance_ratio_.shape[0] + pca_r.explained_variance_ratio_.shape[0]
+print("The number of components used to achieve ", n_var, "variance : ", totalNumComp, ".")
