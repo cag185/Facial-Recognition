68474ebc9f240434a33edf9f323077ba2dbd8258
cag185 <cag185@pitt.edu>
2023-11-08 14:09:57 -0500

    got the bare bones of svm

--- Facial-Detection/SVM_model.py
+++ Facial-Detection/SVM_model.py
@@ -4,12 +4,12 @@
 from sklearn.svm import LinearSVC
 import os
 from PIL import Image
-import numpy as np 
-import matplotlib.pyplot as plt 
-from sklearn import svm 
-from sklearn.model_selection import GridSearchCV 
-from sklearn.model_selection import train_test_split 
-from sklearn.metrics import accuracy_score 
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn import svm
+from sklearn.model_selection import GridSearchCV
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
 from sklearn.metrics import classification_report
 import pandas as pd
 
@@ -26,6 +26,7 @@ label_arr = []
 person_label = 0
 # per each person that has a facial profile
 for person in face_list:
+    print(f"loading....person: {person}")
     # for each image in that profile
     image_dir = parent_folder + person + "/"
     img_list = os.listdir(image_dir)
@@ -37,10 +38,10 @@ for person in face_list:
         curr_image = Image.open(full_path)
         curr_image_nparray = np.array(curr_image)
         # flatten the image
-        flat_data_arr.append(curr_image_nparray.flatten());
+        flat_data_arr.append(curr_image_nparray.flatten())
         # append it to a flattened array
         label_arr.append(person_label)
-    
+    print(f"loaded person: {person} successfully")
     # at the end, add 1 to the iterator
     person_label += 1
 
@@ -48,9 +49,48 @@ for person in face_list:
 label_data = np.array(label_arr)
 print("done preprocessing")
 
+# convert the flat data array into np array
+flat_data = np.array(flat_data_arr)
+
 # create a pandas dataframe out of the data
-df = pd.DataFrame(flat_data_arr)
+df = pd.DataFrame(flat_data)
 df['Label'] = label_data
+
+print(df.shape)
+print("done converting to data frame")
+
+# separate the input features and the labels from the dataframe
+x = df.iloc[:, :-1]
+y = df.iloc[:, -1]
+
+# split the data into testing and training data
+x_train, x_test, y_train, y_test = train_test_split(
+    x, y, test_size=0.20, random_state=77, stratify=y)
+
+# define the params for gridSearchCv
+param_grid = {'C': [0.1, 1, 10, 100],
+              'gamma': [0.0001, 0.001, 0.1, 1],
+              'kernel': ['rbf', 'poly']}
+
+# create the SVM classified
+svc = svm.LinearSVC(probability=True)
+
+# create a model using the grid parameters we defined
+model = GridSearchCV(svc, param_grid=param_grid, verbose=1)
+
+# fit the model to the data
+model.fit(x_train, y_train)
+
+print("The model has been successfully trained")
+
+# test the model using the testing data
+y_pred = model.predict(x_test)
+
+# calculate the accuracy of the model
+# compare the actual vs the prediction
+accurary = accuracy_score(y_pred, y_test)
+print(f"The model accuracy for hudson vs caleb is {accuracy * 100}% accurate")
+
 
 # 1. Load the images and convert to data frame
 # 2. Separate image into features and labels
@@ -58,6 +98,4 @@ df['Label'] = label_data
 # 4. Build and train the SVM
 # 5. Model evaulation (accuraccy)
 # 6. Using model for prediction
-#  
-
-
+#
