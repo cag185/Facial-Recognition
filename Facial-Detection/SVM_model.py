7037e9b9925edca3b0cc792539a7f7576e4da6ca
cag185 <cag185@pitt.edu>
2023-11-12 19:51:25 -0500

    Got everything working but my own face doesnt work well with the test

--- Facial-Detection/SVM_model.py
+++ Facial-Detection/SVM_model.py
@@ -7,89 +7,127 @@ from PIL import Image
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn import svm
-# from sklearn.model_selection import GridSearchCV
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 from sklearn.metrics import classification_report
 import pandas as pd
 from tqdm import tqdm
-
-# @TODO - need to add in the data binding so that the variables are accessable in each function
+import pickle
+import matplotlib.pyplot as plt
+import cv2
 
 # create a class for the SVM model
 # on init, the class will load the data and train the model
 
-# import the data as an array
-parent_folder = "C:/Users/17578/Desktop/School/Class Files/Fall 2023/ECE 1896 - Senior Design/Facial-Recognition/Facial-Profile-Databank/"
-face_list = os.listdir(parent_folder)
-# import array -- holds the flattened images
-flat_data_arr = []
-# label array -- holds the label of the images
-label_arr = []
 
-# iterating var
-person_label = 0
-# per each person that has a facial profile
-for person in face_list:
-    print(f"loading....person: {person}")
-    # for each image in that profile
-    image_dir = parent_folder + person + "/"
-    img_list = os.listdir(image_dir)
+class SVM_facial_detection():
+    # import the data as an array
+    def train_model(self):
+        parent_folder = "C:/Users/17578/Desktop/School/Class Files/Fall 2023/ECE 1896 - Senior Design/Facial-Recognition/Facial-Profile-Databank/"
+        face_list = os.listdir(parent_folder)
+        # import array -- holds the flattened images
+        flat_data_arr = []
+        # label array -- holds the label of the images
+        label_arr = []
 
-    for image in img_list:
-        full_path = image_dir + image
+        # iterating var
+        person_label = 0
+        # per each person that has a facial profile
+        for person in face_list:
+            print(f"loading....person: {person}")
+            # for each image in that profile
+            image_dir = parent_folder + person + "/"
+            img_list = os.listdir(image_dir)
 
-        # open the image as an array
-        curr_image = Image.open(full_path)
-        curr_image_nparray = np.array(curr_image)
-        # flatten the image
-        flat_data_arr.append(curr_image_nparray.flatten())
-        # append it to a flattened array
-        label_arr.append(person_label)
-    print(f"loaded person: {person} successfully")
-    # at the end, add 1 to the iterator
-    person_label += 1
+            for image in img_list:
+                full_path = image_dir + image
 
-    # at this point the two arrays from the start should be np arrays
-    label_data = np.array(label_arr)
-    print("done preprocessing")
+                # open the image as an array
+                curr_image = Image.open(full_path)
+                curr_image_nparray = np.array(curr_image)
+                # flatten the image
+                flat_data_arr.append(curr_image_nparray.flatten())
+                # append it to a flattened array
+                label_arr.append(face_list[person_label])
+            print(f"loaded person: {person} successfully")
+            # at the end, add 1 to the iterator
+            person_label += 1
 
-    # convert the flat data array into np array
-    flat_data = np.array(flat_data_arr)
+            # at this point the two arrays from the start should be np arrays
+            label_data = np.array(label_arr)
+            print("done preprocessing")
 
-    # create a pandas dataframe out of the data
-    df = pd.DataFrame(flat_data)
-    df['Label'] = label_data
+            # convert the flat data array into np array
+            flat_data = np.array(flat_data_arr)
 
-    print(df.shape)
-    print("done converting to data frame")
+            # create a pandas dataframe out of the data
+            df = pd.DataFrame(flat_data)
+            df['Label'] = label_data
 
-    # separate the input features and the labels from the dataframe
-    x = df.iloc[:, :-1]
-    y = df.iloc[:, -1]
+            print(df.shape)
+            print("done converting to data frame")
 
-# split the data into testing and training data
-x_train, x_test, y_train, y_test = train_test_split(
-    x, y, test_size=0.20, random_state=77, stratify=y)
-# assing this data to self
-# self.x_train = x_train
-# self.x_test = x_test
-# self.y_train = y_train
-# self.y_test = y_test
+            # separate the input features and the labels from the dataframe
+            x = df.iloc[:, :-1]
+            y = df.iloc[:, -1]
 
-# create the SVM classifier
-lsvc = svm.LinearSVC(verbose=1, dual="auto", max_iter=10000)
-print("the classifier has been created")
-# fit the model to the data
-(lsvc.fit(x_train, y_train))
-print("The classifier has been trained")
-# assign this to self
-# self.lsvc = lsvc
+        # split the data into testing and training data
+        x_train, x_test, y_train, y_test = train_test_split(
+            x, y, test_size=0.20, random_state=77, stratify=y)
+        # assing this data to self
+        # self.x_train = x_train
+        # self.x_test = x_test
+        # self.y_train = y_train
+        # self.y_test = y_test
 
-# test the model using the testing data
-y_pred = (lsvc.predict(x_test))
+        # create the SVM classifier
+        lsvc = svm.LinearSVC(verbose=1, dual="auto", max_iter=10000)
+        print("the classifier has been created")
+        # fit the model to the data
+        (lsvc.fit(x_train, y_train))
+        print("The classifier has been trained")
 
-# calculate the accuracy of the model
-# compare the actual vs the prediction
-accuracy = accuracy_score(y_pred, y_test)
-print(f"The model accuracy is {accuracy * 100}% accurate")
+        self.lsvc = lsvc
+
+        # # test the model using the testing data
+        # this is for the whole data brought in
+        y_pred = (lsvc.predict(x_test))
+        # compare the actual vs the prediction
+        # this is for the whole data brought in
+        accuracy = accuracy_score(y_pred, y_test)
+
+        # once we have created the model, we want to save it
+        with open('SVM_model.pkl', 'wb') as file:
+            pickle.dump(lsvc, file)
+
+    # create the function for prediction/accuracy
+    def predict(self, x_data):
+        # load the model from storage
+        with open('SVM_model.pkl', 'rb') as file2:
+            lsvc_new = pickle.load(file2)
+            return lsvc_new.predict(x_data)
+
+
+# create an instance of the object
+svm_object = SVM_facial_detection()
+# svm_object.train_model()
+
+# function for loading in a file and converting it into side
+# new_photo = "C:/Users/17578/Desktop/School/Class Files/Fall 2023/ECE 1896 - Senior Design/Facial-Recognition/Test-Images/Caleb_Test_1.jpg"
+# new_photo = "C:/Users/17578/Desktop/School/Class Files/Fall 2023/ECE 1896 - Senior Design/Facial-Recognition/Test-Images/Caleb_Test_2.jpg"
+new_photo = "C:/Users/17578/Desktop/School/Class Files/Fall 2023/ECE 1896 - Senior Design/Facial-Recognition/Test-Images/Caleb_Test_3.jpg"
+
+
+def loadNewPhoto(new_photo):
+    # change the photo to be an nparray
+    opened_img = np.asarray(Image.open(new_photo).resize((500, 500)))
+    # reshape
+    plt.imshow(opened_img)
+    plt.show()
+    flattened_img = (opened_img.flatten()).reshape(1, -1)
+    new_pred = svm_object.predict(flattened_img)
+    # output the prediction
+    print(f"The label for the input data was {new_pred}")
+
+
+loadNewPhoto(new_photo=new_photo)
