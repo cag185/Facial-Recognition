f9e9823ae4a316e526b1ad5a69695e21d484b8fe
cag185 <cag185@pitt.edu>
2023-11-30 21:08:53 -0500

    cam

--- Facial-Detection/TestCam.py
+++ Facial-Detection/TestCam.py
@@ -1,20 +1,20 @@
 import cv2
-from picamera2 import Picamera2, Preview
+from camera2 import camera2, Preview
 import time
 import os
 
-cam = Picamera2()
+cam = camera2()
 cam.preview_configuration.main.size = (1280, 720)
-piCam.framerate = 500
-piCam.preview_configuration.main.format = "RGB888"
-piCam.preview_configuration.main.align()
-piCam.configure("preview")
-piCam.start()
+cam.framerate = 500
+cam.preview_configuration.main.format = "RGB888"
+cam.preview_configuration.main.align()
+cam.configure("preview")
+cam.start()
 
 # grab a frame
 print("prepate to take a pic.....")
 time.sleep(1)
-frame = piCam.capture_array()
+frame = cam.capture_array()
 gray = frame.convert('L')
 new_test_img = "new_testing_img.png"
 cv2.imwrite(new_test_img, gray)
