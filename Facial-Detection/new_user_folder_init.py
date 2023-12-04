99773b57725947d3fbf84793a797bf4b9700d305
cag185 <cag185@pitt.edu>
2023-12-03 21:18:58 -0500

    cam_port

--- Facial-Detection/new_user_folder_init.py
+++ Facial-Detection/new_user_folder_init.py
@@ -11,7 +11,7 @@ import time
 
 def recordVideoLaptop(file_dest):
     # start a recording with CV2
-    cap = cv2.VideoCapture(1)
+    cap = cv2.VideoCapture('/dev/video0')
     # check if camera opened
     if not cap.isOpened():
         print("Error: could not open camera.")
