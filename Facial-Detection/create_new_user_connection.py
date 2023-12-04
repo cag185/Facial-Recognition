763d3cad011f385fc5d6473ca844f2d73a000b83
cag185 <cag185@pitt.edu>
2023-12-04 01:34:38 -0500

    h

--- Facial-Detection/create_new_user_connection.py
+++ Facial-Detection/create_new_user_connection.py
@@ -2,21 +2,19 @@ import subprocess
 import sys
 import os
 
-folder_name_to_create = sys.argv[0]
+folder_name = sys.argv[0]
 
 # Replace '/path/to/venv' with the actual path to your virtual environment
-venv_path = '~/Desktop/Facial-Recognition/venv/bin/activate'
-
-# Run the Python script
-# Replace with the actual path to your Python script
-script_to_run = '~/Desktop/Facial-Recognition/Facial-Detection/'
+venv_path = '~/Desktop/Facial-Recognition/venv'
 
 # Activate the virtual environment directly without using source
-activate_command = venv_path
+activate_command = os.path.join(venv_path, 'bin', 'activate_this.py')
 exec(open(activate_command).read(), {'__file__': activate_command})
 
 # Run the Python script
-python_command = f'python {script_to_run} {folder_name_to_create}'
+# Replace with the actual path to your Python script
+script_to_run = '~/Desktop/Facial-Recognition/Facial-Detection/new_user_folder_init.py'
+python_command = f'python {script_to_run} {folder_name}'
 
 # Combine commands and execute
 full_command = f'{python_command}'
